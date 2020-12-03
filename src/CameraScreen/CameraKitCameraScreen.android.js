9ac329f9193e309fb43a0001fec824b6b8d918d9
karansingh82888 <61904789+karansingh82888@users.noreply.github.com>
2020-12-03 14:45:14 +0530

    Update CameraKitCameraScreen.android.js

--- src/CameraScreen/CameraKitCameraScreen.android.js
+++ src/CameraScreen/CameraKitCameraScreen.android.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import CameraScreenBase from './CameraKitCameraScreenBase';
 
 export default class CameraScreen extends CameraScreenBase {
   renderGap() {
     return (
-      <View style={{flex: 10, flexDirection: 'column'}}/>
+      <View style={{ flex: 10, flexDirection: 'column' }} />
     );
   }
 
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: 'transparent' }} {...this.props}>
-        {this.renderCamera()}
-        {this.renderTopButtons()}
-        {this.renderGap()}
-        <View style={{position:"absolute",alignSelf:"center",bottom:2}}>{this.renderBottomButtons()}</View>
+        <View style={{ flex: 0.9 }}>
+          {this.renderCamera()}
+          {this.renderTopButtons()}
+          {this.renderGap()}
+        </View>
+        <View style={{ flex: 0.1 }}>
+          {this.renderBottomButtons()}
+        </View>
       </View>
     );
   }
