57df04ea0eba190f035f5a927cf4b583970397e6
karansingh82888 <61904789+karansingh82888@users.noreply.github.com>
2020-12-03 17:51:53 +0530

    Update CameraKitCameraScreen.ios.js

--- src/CameraScreen/CameraKitCameraScreen.ios.js
+++ src/CameraScreen/CameraKitCameraScreen.ios.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import CameraScreenBase from './CameraKitCameraScreenBase';
 
 export default class CameraScreen extends CameraScreenBase {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: 'black' }} {...this.props}>
-        
         {this.renderCamera()}
-    <View style={{position:"absolute",alignSelf:"flex-end"}}>{this.renderTopButtons()}</View>
+        <View style={{ position: "absolute", alignSelf: "flex-end" }}>{this.renderTopButtons()}</View>
         {this.renderRatioStrip()}
+        <View style={{position:"absolute",alignSelf:"center",bottom:40}}>
         {this.renderBottomButtons()}
+        </View>
       </View>
     );
   }
